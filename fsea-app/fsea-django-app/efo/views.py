3ab7aa41c5bc6600aed486e615eb73e680fb1fd1
ZGiovas <64550111+ZGiovas@users.noreply.github.com>
2025-02-09 16:36:49 +0200

    feat: update API for sorting, filtering and searching

--- fsea-app/fsea-django-app/efo/views.py
+++ fsea-app/fsea-django-app/efo/views.py
@@ -1,10 +1,31 @@
-from rest_framework import generics
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from .models import EFOTerm
 from .serializers import EFOTermSerializer
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10  
+    page_size_query_param = "page_size" 
+
+    def get_paginated_response(self, data):
+        return Response({
+            "total_count": self.page.paginator.count,
+            "total_pages": self.page.paginator.num_pages, 
+            "current_page": self.page.number, 
+            "page_size": self.page.paginator.per_page,
+            "results": data 
+        })
 
 class EFOTermList(generics.ListAPIView):
     queryset = EFOTerm.objects.all()
     serializer_class = EFOTermSerializer
+    pagination_class = CustomPagination
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter] 
+    filterset_fields = ['is_obsolete', 'has_children', 'is_root', 'lang'] 
+    ordering_fields = ['ontology_id','label', 'description', 'obo_id']
+    search_fields = ['ontology_id', 'label', 'description', 'obo_id', 'synonyms__synonym']
 
 class EFOTermDetail(generics.RetrieveAPIView):
     queryset = EFOTerm.objects.all()
--- fsea-app/fsea-django-app/fsea/settings.py
+++ fsea-app/fsea-django-app/fsea/settings.py
@@ -38,10 +38,22 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'efo'
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter",
+        "rest_framework.filters.SearchFilter",
+    ],
+}
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +136,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    # For dev reasons allow localhost at default port of Vite
+    "http://localhost:5173",  
+]
