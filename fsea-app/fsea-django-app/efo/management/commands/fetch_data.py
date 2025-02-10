44bdc8242be75de938f95ec2e13113c0bd0a640e
ZGiovanopoulos <zisis_giovanopoulos@hotmail.com>
2025-02-10 23:50:28 +0200

    feat: docker initilization finalized

--- fsea-app/fsea-django-app/efo/management/commands/fetch_data.py
+++ fsea-app/fsea-django-app/efo/management/commands/fetch_data.py
@@ -1,15 +1,15 @@
 from django.core.management.base import BaseCommand, CommandError
-from utils import fetch_efo_terms
+from efo.utils import fetch_efo_terms
 
 
 class Command(BaseCommand):
     help = "Fetching EFO Data"
 
     def add_arguments(self, parser):
-        parser.add_argument("max_pages", nargs=1, type=int)
+        parser.add_argument("max_pages", nargs="?", type=int, default=4)
 
     def handle(self, *args, **options):
         fetch_efo_terms(options['max_pages'])
         self.stdout.write(
-            self.style.SUCCESS(f'Successfully downloaded data for {options['max_pages']} pages')
+            self.style.SUCCESS(f'Successfully downloaded data for {options["max_pages"]} pages')
         )
